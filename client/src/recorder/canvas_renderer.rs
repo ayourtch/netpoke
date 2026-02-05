d526205e53c19956f7d68e742f1d7162e495cc6a
Andrew Yourtchenko <ayourtch@gmail.com>
2026-02-05 09:18:45 +0100

    tweak the marquee
    Change-Id: I5b3c4393ce22b68aed0d237e338256052a50b81c

--- client/src/recorder/canvas_renderer.rs
+++ client/src/recorder/canvas_renderer.rs
@@ -180,7 +180,7 @@ impl CanvasRenderer {
     }
 
     fn draw_marquee(&self, canvas_width: f64) -> Result<(), JsValue> {
-        let text = "NetPoke - Network Measurement Tool";
+        let text = "NetPoke - Network Measurement Tool - https://netpoke.com/";
         let font_size = 20.0;
         let marquee_y = 30.0;
 
